6dce6b7a1cc4ce8987c5c652c4f276c94e082ae0
Julian <julian.rivera@mail.escuelaing.edu.co>
2018-09-10 15:44:00 -0500

    Correccion error de puerto

--- src/main/java/co/edu/escuelaing/arem/designprimer/SparkWebApp.java
+++ src/main/java/co/edu/escuelaing/arem/designprimer/SparkWebApp.java
@@ -14,6 +14,7 @@ import static spark.Spark.*;
 public class SparkWebApp {
 
     public static void main(String[] args) {
+        port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
     }
 
