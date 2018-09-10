5ab724d5121eded5c14ebe34061536c56dc258ea
Julian <julian.rivera@mail.escuelaing.edu.co>
2018-09-10 15:33:15 -0500

    Preparacion para desplegar en heroku

---
+++ src/main/java/co/edu/escuelaing/arem/designprimer/SparkWebApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arem.designprimer;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author 2114816
+ */
+public class SparkWebApp {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e.on localhost)
+    }
+}
